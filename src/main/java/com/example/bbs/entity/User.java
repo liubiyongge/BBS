c6d70f08b11507acc3094caa6a5ab10de46e635a
liubiyongge <liubiyongge@163.com>
2019-11-29 17:37:37 +0800

    userDao补充

--- src/main/java/com/example/bbs/entity/User.java
+++ src/main/java/com/example/bbs/entity/User.java
@@ -1,4 +1,105 @@
 package com.example.bbs.entity;
 
+import java.util.Date;
+
 public class User {
+    private int userId;
+    private String userName;
+    private String password;
+    private int sex;
+    private int credit;
+    private String telephone;
+    private String profilePhoto;
+    private String briefIntro;
+    private String location;
+    private int type;
+    private Date birthday;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getBriefIntro() {
+        return briefIntro;
+    }
+
+    public void setBriefIntro(String briefIntro) {
+        this.briefIntro = briefIntro;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 }
