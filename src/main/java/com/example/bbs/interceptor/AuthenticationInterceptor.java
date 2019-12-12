bf1985516f67e5e0519464825c2f57e0b87f8d17
liubiyongge <liubiyongge@163.com>
2019-12-12 21:55:15 +0800

    修复用户信息跳转页面bug

--- src/main/java/com/example/bbs/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/example/bbs/interceptor/AuthenticationInterceptor.java
@@ -26,9 +26,7 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
         }
         //accessUri   ==   "/page/1231231.htm"
         //原语句是if(request.getRequestURI.equals("/bbs_war/login")，路径应该有问题
-        System.out.println(request.getRequestURI());
         boolean isUser=request.getRequestURI().substring(0,6).matches("/User/*");
-        System.out.println(isUser);
         boolean isWebMaster=request.getRequestURI().matches("/webMaster/*");
         boolean isAdmin=request.getRequestURI().matches("/admin/*");
         if(request.getRequestURI().equals("/admin/adminLogin")||request.getRequestURI().equals("/User/login")
